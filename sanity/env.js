8c962b09cabca2965d2744078817693b7ddba1c9
Sam Melius Oxford <samsjmelius@gmail.com>
2025-12-11 14:35:05 -0800

    update next.js per Vercel 6

--- sanity/env.js
+++ sanity/env.js
@@ -5,7 +5,7 @@
 // export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 // export const useCdn = false
 
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-09-23';
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-11-01';
 export const dataset = 'production';
-export const projectId = 'mlqi9vxv';
+export const projectId = '0cr3aoug';
 export const useCdn = false;
